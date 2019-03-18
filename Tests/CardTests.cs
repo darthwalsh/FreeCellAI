e88a21518f2d540575ae52ab4a760a8829e2cbc7
Carl Walsh <darthwalsh@gmail.com>
2019-03-17 18:00:56 -0700

    added test library

---
+++ Tests/CardTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class CardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("Hello World");
+        }
+    }
+}
