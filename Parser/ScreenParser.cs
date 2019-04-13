46471a9a0c8cebbbc9115672e1827b6719a6cb87
Carl Walsh <darthwalsh@gmail.com>
2019-04-13 11:02:31 -0700

    Add blank Parser and GUI projects

---
+++ Parser/ScreenParser.cs
@@ -0,0 +1,24 @@
+﻿using ImageParse;
+using System;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class ScreenParser
+    {
+        IAsyncBitmap image;
+        Finder finder;
+        public ScreenParser(IAsyncBitmap image)
+        {
+            this.image = image;
+            finder = new Finder(image);
+        }
+
+        public async Task<string> Parse()
+        {
+            
+
+            return "";
+        }
+    }
+}
